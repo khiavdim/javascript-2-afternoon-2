341c02d0f73d5764568757d055697123a6114526
KhiavDim Lee <khiavdim@KhiavDims-MacBook-Pro.local>
2019-06-05 15:24:41 -0600

    working on prob4 in practice.js

--- practice.js
+++ practice.js
@@ -18,7 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
+function first(arr){
+  return arr[0]
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -33,6 +35,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
+function last(arr) {
+  return arr[arr.length - 1]
+}
 
 
 
@@ -48,7 +53,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+function looper(family){
+  for (let i = 0; i < family.length; i++){
+    alert(family[i])
+  }
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -63,6 +72,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
+function reversedLooper(letters){
+  for (let i = 0; i < letters.length; i--){
+    alert(letters[i])
+  }
+}
 
 
 
@@ -78,6 +92,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
+// function evenFinder(nums){
+//   let evenNums = []
+//   for (let i = 0; i < nums.length; i++){
+//     if (nums[i] % 2 == true){
+//       nums[i] = evenNums.push
+//     }
+//   }
+//   return evenNums
+// }
 
 
 
