496fd0a153a18bd34a6cb895783aedd0b752038c
KhiavDim Lee <khiavdim@KhiavDims-MacBook-Pro.local>
2019-06-05 15:29:52 -0600

    working on prob5

--- practice.js
+++ practice.js
@@ -73,7 +73,7 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 
 //Code Here
 function reversedLooper(letters){
-  for (let i = 0; i < letters.length; i--){
+  for (let i = letters.length-1; i >= 0 ; i--){
     alert(letters[i])
   }
 }
